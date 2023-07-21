d040a025a2c85a937dddac829bd0ddd8b97272f7
Daniel Felix <felixdaniel9@gmail.com>
2023-07-21 09:53:35 -0300

    Add currency details page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,8 +1,91 @@
+import {useState, useEffect} from 'react';
+import styles from './detail.module.css';
+import { useParams } from 'react-router-dom';
+
+interface CoinProps{
+  symbol: string;
+  name: string;
+  price: string;
+  market_cap: string;
+  low_24h: string;
+  high_24h: string;
+  total_volume_24h: string;
+  delta_24h: string;
+  formatedPrice: string;
+  formatedMarket: string;
+  formatedLowPrice: string;
+  formatedHighPrice: string;
+  error?: string;
+}
 
 export function Detail(){
+  const {cripto} = useParams();
+
+  const [detail, setDetail] = useState<CoinProps>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData(){
+      await fetch(`https://sujeitoprogramador.com/api-cripto/coin/?key=d7c378a3be95d4b5&pref=BRL&symbol=${cripto}`)
+      .then(res => res.json())
+      .then((data: CoinProps) => {
+        
+        let price = Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+
+        const resultData = {
+          ...data,
+          formatedPrice: price.format(Number(data.price)),
+          formatedMarket: price.format(Number(data.market_cap)),
+          formatedLowPrice: price.format(Number(data.low_24h)),
+          formatedHighPrice: price.format(Number(data.high_24h)),
+        }
+        
+        setDetail(resultData);
+        setLoading(false);
+      })
+    }
+
+    getData();
+  }, [cripto])
+
+  if(loading){
+    return(
+      <div className={styles.container}>
+        <h4 className={styles.center}>Carregando...</h4>
+      </div>
+    )
+  }
+
   return(
     <>
-      <h1>Página de Detalhe</h1>
+      <div className={styles.container}>
+        <h1 className={styles.center}>{detail?.name}</h1>
+        <p className={styles.center}>{detail?.symbol}</p>
+
+        <section className={styles.content}>
+          <p>
+            <strong>Preço:</strong> {detail?.formatedPrice}
+          </p>
+          <p>
+            <strong>Maior preço:</strong> {detail?.formatedHighPrice}
+          </p>
+          <p>
+            <strong>Menor preço:</strong> {detail?.formatedLowPrice}
+          </p>
+          <p>
+            <strong>Delta 24h:</strong>
+            <span className={Number(detail?.delta_24h) >= 0 ? styles.profit : styles.loss}>
+              {detail?.delta_24h}
+            </span> 
+          </p>
+          <p>
+            <strong>Valor mercado:</strong> {detail?.formatedMarket}
+          </p>
+        </section>
+      </div>
     </>
   )
 }
